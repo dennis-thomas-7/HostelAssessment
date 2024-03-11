5856390866900aa051577c7e2b84e8b1499dd612
Dennis Thomas <dennis.reubro@gmail.com>
2024-03-11 12:34:25 +0000

    removed unwanted files

---
+++ app/src/test/java/com/network/hostelworld/presentation/propertylisting/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.network.hostelworld.presentation.propertylisting
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
